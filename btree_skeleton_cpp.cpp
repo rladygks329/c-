27d2552120844951fa4617054d071c5232b526e1
Kim Yo Han <64533351+rladygks329@users.noreply.github.com>
2020-12-19 18:59:26 +0900

    Add files via upload

--- btree_skeleton_cpp.cpp
+++ btree_skeleton_cpp.cpp
@@ -7,7 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include<string.h>
 #include<stack>
+#include<algorithm>
 using namespace std;
 /**
  * BTNode represents a single node in B-tree.
@@ -16,7 +18,6 @@ typedef struct BTNode {
   int           n,  *K;
   struct BTNode **P;
 } BTNode;
-
 /**
  * getBTNode retruns a new BTNode.
  * @param m: number of branch points in B-tree
@@ -25,17 +26,24 @@ typedef struct BTNode {
 BTNode *getBTNode(int m) {
   BTNode *node  = (BTNode *)malloc(sizeof(BTNode));
   node -> n     = 0;
-  node -> K     = (int *)malloc(sizeof(int) * (m-1));
-  node -> P     = (BTNode **)malloc(sizeof(BTNode *) * m);
-
-  for(int i=0;i<m;i++)
-  {
-      node -> P[i] =  NULL;
-  }
+  node -> K = (int *)calloc(m-1,sizeof(int));
+  node -> P = (BTNode**)calloc(m, sizeof(BTNode*));
   return node;
 }
 
 typedef BTNode *BTree;
+void printBTNode(BTNode* a)
+{
+    if(a!= NULL)
+    {
+        for(int i=0;i<a->n;i++)
+        {
+            //printBTNode(a->P[i]);
+            cout <<a->K[i]<<" ";
+        }
+        //printBTNode(a->P[a->n]);
+    }
+}
 /**
  * insertBT inserts newKey into T.
  * @param T: a B-tree
@@ -45,124 +53,127 @@ typedef BTNode *BTree;
 void insertBT(BTree *T, int m, int newKey) {
     if(*T == nullptr)//empty
     {
-        *T  = getBTNode(m);
+        (*T)  = getBTNode(m);
         (*T) ->  n = 1;
         (*T) ->  K[0] = newKey;
         return;
     }
+
+    //searching
     BTNode * x = *T;
     int index;
     stack<BTNode*> path;
-    stack<int> pathi;
+
     while( x != NULL)
     {
         int i = 0;
         while( i< x->n && newKey > x->K[i])
             i++;
+        if(i <x->n && newKey == x->K[i]) return;
         path.push(x);
-        pathi.push(i);
         x = x->P[i];
-    }//end location finding
+    }
+    stack<BTNode*> split;
 
-    BTNode * tempNode = getBTNode(m+1);
-    BTNode *y = getBTNode(m);
-    stack<BTNode *> split;
-    bool finished = false;
-
-    x = path.top();
-    path.pop();
-    index = pathi.top();
-    pathi.pop();
-
-    while(!finished){
+    do{
+        x = path.top();
+        path.pop();
+        index =0;
+        for(int i=0;i<x->n;i++){ if(newKey > x->K[i]) index++;}
         if(x->n < m-1 ) // can insert without split;
         {
-            for(int i = x->n -1; i >=  index; i--) //n ->index make a space
-            {
-                x->K[i+1] = x->K[i];
-                x->P[i+1] = x->P[i];
-            }
-            x->K[index] = newKey; //insert
-            if(!split.empty())
-            {
-                x->P[index+1] = split.top();
-                split.pop();
-            }
+            memmove(x->K+index+1,x->K+index,sizeof(int)*(x->n-index));
+            x->K[index] = newKey;
+            memmove(x->P+index+2,x->P+index+1,sizeof(BTNode*)*(x->n-index));
+            if(split.empty()) {x->P[index+1] = NULL;}
+            else {x->P[index+1] = split.top(); split.pop();}
             x->n++;
-            finished = true;
             break;
         }
-        else
+        else// make tempNode and  split it
         {
-            if(x->P[0] == NULL) //leaf_node
+            BTNode * tempNode = getBTNode(m+1);
+            BTNode *y = getBTNode(m);
+            memcpy(tempNode->K,x->K,sizeof(int)*index);
+            tempNode->K[index] = newKey;
+            memcpy(tempNode->K+index+1,x->K+index, sizeof(int)*(m-1-index));
+            if(x->P[0]!=NULL)
             {
-                for(int i = 0; i<index; i++)
-                {
-                    tempNode ->K[i] = x->K[i];
-                }
-                tempNode->K[index] = newKey;
-                for(int i = index+1;i<m;i++)
-                {
-                    tempNode ->K[i] = x->K[i-1];
-                }
-            }
-            else//not leaf_node
-            {
-                for(int i = 0; i<index; i++)
-                {
-                    tempNode ->K[i] = x->K[i];
-                    tempNode ->P[i] = x->P[i];
-                }
-                tempNode ->K[index] = newKey;
-                tempNode->P[index] = x->P[index];
-
-                tempNode ->K[index+1] = x->K[index+1];
-                tempNode ->P[index+1] = split.top();
+                memcpy(tempNode->P,x->P,sizeof(BTNode*)*(index+1));
+                tempNode->P[index+1] = split.top();
                 split.pop();
-                for(int i =index+2;i<m;i++)
-                {
-                    tempNode->K[i] = x->K[i-1];
-                    tempNode->P[i] = x->P[i-1];
-                }
+                if(m>index) memcpy(tempNode->P+index+2,x->P+index+1, sizeof(BTNode*)*(m-index));
+            }
+            int mid = m/2;
+            newKey =  tempNode->K[mid];
+            //make x
+            x->n = 0;
+            for(int i=0;i<mid;i++)
+            {
+                x->K[i] = tempNode->K[i];
+                x->P[i] = tempNode->P[i];
+                x->n++;
+            }
+            x->P[mid] =tempNode->P[mid];
+            //make y
+            y->n = 0;
+            for(int i=mid+1;i<m;i++)
+            {
+                y->K[i-mid-1] = tempNode->K[i];
+                y->P[i-mid-1] = tempNode->P[i];
+
+                x->K[i] = 0;
+                x->P[i] = NULL;
+                y->n++;
+            }
+            y->P[y->n] = tempNode->P[m];
+            split.push(y);
+            if(path.empty())
+            {
+                (*T) = getBTNode(m);
+                (*T) ->K[0] = newKey;
+                (*T) ->P[0] = x;
+                (*T) ->P[1] = y;
+                (*T) -> n = 1;
             }
         }
-        //split
-        int mid = m/2;
-        newKey = tempNode->K[mid];
-        for(int i =0; i<mid;i++)
-        {
-            x->K[i] = tempNode ->K[i];
-            x->P[i] = tempNode ->P[i];
-        }
-        for(int i = mid; i<m; i++)
-        {
-            y ->K[i-mid] = tempNode ->K[i+1];
-            y->P[i-mid] = tempNode ->P[i+1];
-            y->n++;
+    }while(!path.empty());//end while
+}//end insert
 
-            x->K[i] = 0;
-            x->P[i] = NULL;
-        }
-        x->n = mid-1;
-        split.push(y);
-        if(!path.empty())
+bool find_Sibling(BTNode * node , int index, int m)
+{
+    bool result = false;
+    if(node ->n == index )
+    {
+        if(node->P[index-1]->n > m/2)
         {
-            x = path.top();
-            path.pop();
-            index = pathi.top();
-            pathi.pop();
-        }
-        else
-        {
-            (*T) = getBTNode(m);
-            (*T) ->K[0] = newKey;
-            (*T) ->P[0] = x;
-            (*T) ->P[1] = y;
-            finished = true;
+            index--;
+            result = true;
         }
     }
+    else if(index == 0)
+    {
+        if(node->P[index+1]->n >m/2)
+        {
+            index++;
+            result =  true;
+        }
+    }
+    else
+    {
+        if(node->P[index-1]->n > m/2)
+        {
+            index--;
+            result = true;
+        }
+        if(node->P[index+1]->n >m/2)
+        {
+            index++;
+            result = true;
+        }
+    }
+    return result;
 }
-
 /**
  * deleteBT deletes oldKey from T.
  * @param T: a B-tree
@@ -170,7 +181,118 @@ void insertBT(BTree *T, int m, int newKey) {
  * @param oldKey: a key to delete
  */
 void deleteBT(BTree *T, int m, int oldKey) {
-  /* write your code here */
+    BTNode * x = *T;
+    stack<BTNode*> path;
+
+    int index;
+    while( x != NULL)
+    {
+        index = 0;
+        while( index< x->n && oldKey > x->K[index])
+            index++;
+        if(index <x->n && oldKey == x->K[index]) break;
+        path.push(x);
+        x = x->P[index];
+    }
+    if(x == NULL) return;
+
+    BTNode * internalNode;
+    int temp;
+    if(x->P[0] != NULL)
+    {
+        internalNode = x;
+        path.push(x);
+        x = x->P[index];
+        while(x->P[0] != NULL)
+        {
+            path.push(x);
+            x = x->P[0];
+        }
+    }
+    if(x == NULL)//exchange
+    {
+        x = path.top();
+        path.pop();
+        temp =internalNode->K[index];
+        internalNode->K[index] =  x->K[x->n-1];
+        x->K[1] = temp;
+    }
+    BTNode * y;
+    if(!path.empty())
+    {
+        y = path.top();
+        path.pop();
+    }
+    while(!path.empty())
+    {
+        if( x->n >m/2 )
+        {
+            for(int i=0;i<x->n;i++)
+            {
+                if(x->K[i] == oldKey)
+                {
+                    x->K[i] = 0;
+                    x->n--;
+                }
+            }
+            sort(x->K,x->K+x->n);
+            break;
+        }
+        else if(find_Sibling(y,index,m))
+        {
+            int temp = y->K[index-1];
+            int sib;
+            if(y ->n == index )
+            {
+                if(y->P[index-1]->n > m/2)
+                {
+                    sib = index-1;
+                }
+            }
+            else if(index == 0)
+            {
+                if(y->P[index+1]->n >m/2)
+                {
+                    sib = index+1;
+                }
+            }
+            else
+            {
+                if(y->P[index-1]->n > m/2)
+                {
+                    sib = index-1;
+                }
+                if(y->P[index+1]->n >m/2)
+                {
+                    sib = index+1;
+                }
+            }
+            BTNode* sibling = y->P[sib];
+            y->K[index-1] = sibling->K[sibling->n-1];
+            sibling->K[sibling->n-1] = 0;
+            sibling->n--;
+            x->K[x->n] = temp;
+            for(int i=0;i<x->n;i++)
+            {
+                if(x->K[i] == oldKey)
+                {
+                    x->K[i] = 0;
+                    x->n--;
+                }
+            }
+            sort(x->K,x->K+x->n);
+            break;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if(y->n == 0)
+    {
+        (*T) = x;
+        delete y;
+    }
 }
 
 /**
